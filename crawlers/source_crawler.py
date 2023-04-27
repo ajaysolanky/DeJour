638fcb74a50e10e27253234cb7a29740d1eb344d
aalpat1 <aalhad.patankar@gmail.com>
2023-04-27 13:09:51 -0400

    Updated NBA crawler to only have top stories as a source

--- crawlers/source_crawler.py
+++ crawlers/source_crawler.py
@@ -24,12 +24,16 @@ class SourceCrawler(BaseCrawler):
                 if a.url is None or a.url == "":
                     logging.info("skipping article with no url")
                     continue
+
                 if a.title is None or a.title == "":
-                    logging.info(f"skipping article with no title. Url: {a.url}")
-                    continue
+                    logging.info(f"Article has no title. Falling back to url: {a.url}")
+                    title = a.url
+                else:
+                    title = a.title
+                
                 all_news.append({
                     "url": a.url,
-                    "title": a.title
+                    "title": title
                 })
             except:
                 pass
--- crawlers/sources/nba_source.py
+++ crawlers/sources/nba_source.py
@@ -7,10 +7,11 @@ class NBASource(CustomSource):
     BASE_URL = 'https://www.nba.com/'
 
     def set_categories(self):
-        self.categories = [Category(url='https://www.nba.com/news'),
+        self.categories = [
+            # Category(url='https://www.nba.com/news'),
                            Category(url='https://www.nba.com/news/category/top-stories'),
-                           Category(url='https://www.nba.com/news/category/power-rankings'),
-                           Category(url='https://www.nba.com/draft/2023'),
+                        #    Category(url='https://www.nba.com/news/category/power-rankings'),
+                        #    Category(url='https://www.nba.com/draft/2023'),
                         #    Category(url='https://www.nba.com/history')
                            ]
         
